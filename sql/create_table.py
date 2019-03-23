7c6afebbe831a9493259cdd729513424fbe10e45
zhangzhiliang <258983130@qq.com>
2019-03-23 10:35:50 +0800

    create talbe

---
+++ sql/create_table.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+conn = create_engine("mysql+pymysql://root:ztzz910327@localhost:3306/review")
+path = '/home/zhangzhiliang/Documents/my_git/DATA-SCIENTIST-/sql'
+root = []
+dir = []
+file = []
+
+for r, d, f in os.walk(path):
+    root.append(r)
+    dir.append(d)
+    file.append(f)
+
+m = 0
+for f in file[0]:
+    d = path + '/{}'.format(str(f))
+    n = os.path.splitext(file[0][m])
+    if n[1] == '.csv':
+        print(f)
+        df = pd.read_csv('/home/zhangzhiliang/Documents/my_git/DATA-SCIENTIST-/sql/{}'.format(file[0][m]))
+        df.to_sql('{}'.format(n[0]), con=conn, if_exists='replace', index=False)
+    m += 1
