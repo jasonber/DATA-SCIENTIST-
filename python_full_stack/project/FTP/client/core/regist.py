6665dde7695448f9bb0c04a74908e3866b6d83d4
zhang <258983130@qq.com>
2020-02-22 17:27:28 +0800

    notebook FTP

--- python_full_stack/project/FTP/client/core/regist.py
+++ python_full_stack/project/FTP/client/core/regist.py
@@ -2,6 +2,7 @@ def regist(request):
     #? 接收输入用户名提示
     print(request.recv(1024).decode('utf-8'))
     new_id = input(">>>")
+    request.send(new_id.encode('utf-8'))
     #? 注册用户名是否重复
     id_res = request.recv(1024).decode('utf-8')
     while id_res == "用户名重复":
@@ -10,5 +11,6 @@ def regist(request):
     #? 接收输入密码提示
     print(request.recv(1024).decode('utf-8'))
     new_pwd = input(">>>")
+    request.send(new_pwd.encode('utf-8'))
     #? 接收注册结果
     print(request.recv(1024).decode('utf-8'))
