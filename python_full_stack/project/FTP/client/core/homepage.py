e45b41d174e37b66deab0e28fa435eadf1a62758
Jason <258983130@qq.com>
2020-02-22 16:41:08 +0800

    update FTP

--- python_full_stack/project/FTP/client/core/homepage.py
+++ python_full_stack/project/FTP/client/core/homepage.py
@@ -9,9 +9,15 @@ import regist
 
 def homepage(request):
     print(request.recv(1024).decode('utf-8'))
-    flag = input("1 登录\n2 注册\n>>>")
-    request.send(flag.encode('utf-8'))
-    if flag == "1":
-        login.login(request)
-    if flag == "2":
-        regist.regist(request)
+    a = 1
+    while a:
+        flag = input("1 登录\n2 注册\n>>>")
+        request.send(flag.encode('utf-8'))
+        if flag == "1":
+            login.login(request)
+            a = 0
+        elif flag == "2":
+            regist.regist(request)
+            a = 0
+        else:
+            print("输入错误， 请重新输入")
