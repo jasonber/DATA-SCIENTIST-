81ded562f451ab0534f9bcfb6a8ad0f3dd6d9f7b
Jason <258983130@qq.com>
2020-02-26 21:10:56 +0800

    full_stack:FTP debug

--- python_full_stack/project/FTP/server/core/history/server_cmd.py
+++ python_full_stack/project/FTP/server/core/history/server_cmd.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-file_path = "/".join(__file__.split('/')[:-2])
+file_path ="/".join(__file__.split('/')[:-2])
 #? 保证用户的命令只能在服务器的文件存储位置处操作
 usr_path = file_path + '/user_dir'
 os.chdir(usr_path)
@@ -8,17 +8,16 @@ import trans_data
 import struct
 import time
 
-
 def bash(cmd, request):
-    proc = subprocess.Popen(cmd,
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
+    proc = subprocess.Popen(cmd, 
+                            stdin=subprocess.PIPE, 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE, 
                             shell=True)
 
     res = proc.stdout.read()
     err = proc.stderr.read()
-
+    
     #? 不同的命令输出的结果不同，发送与接收的模式也不同
     # ?有结果：成功有结果（1）、，错误报错（1）。无结果：成功无结果（0）
     # ?其中成功无结果无法发送，所以发送指定内容
@@ -31,7 +30,7 @@ def bash(cmd, request):
         header_res = struct.pack('i', err_size)
         request.send(header_res)
         request.send(err)
-    elif res:
+    elif res: 
         request.send('有'.encode('utf-8'))
         res_size = len(res)
         header_res = struct.pack('i', res_size)
@@ -42,14 +41,14 @@ def bash(cmd, request):
         request.send("操作成功".encode('utf-8'))
 
 
-def run_cmd(request):
+def server_cmd(request):
     command = request.recv(1024).decode('utf-8')
     print(command)
     # time.sleep(2)
     try:
         cmd, directory = command.split(" ")
     except ValueError as v:
-        print("接收命令处的错误", v)
+        print("接收命令处的错误",v)
         print('''命令错误\n请按照"命令 目录"的格式输入命令''')
     try:
         if cmd == "查看":
@@ -75,11 +74,14 @@ def run_cmd(request):
             trans_data.recv_file(request)
         elif cmd == "接收":
             trans_data.send_file(request)
-    except UnboundLocalError as u:
+    except UnboundLocalError as u: 
         # print("执行命令时的错误", u)
         pass
 
+def run():
+    command = input('请输入命令:') #? 有四个命令：查看、新建、删除、切换
+    server_cmd(command)
 
 if __name__ == "__main__":
     while 1:
-        run()
+       run()
